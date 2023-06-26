6d3ab8810c455644c4d6250ed3aae234efcf8f8e
Silas Pereira <silasrp@hotmail.com>
2023-06-26 12:40:10 +0100

    Fininshed connecting front and back ends

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,11 +1,35 @@
+import {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import axios  from 'axios';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 import articles from './article-content';
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({upvotes:0, comments:[]});
     const { articleId } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, []);
+
+
     const article = articles.find(article => article.name === articleId);
 
+    // Make a request to the server that we want to upvote an article
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    };
+
+    //Json
     if (!article) {
         return <NotFoundPage />
     }
@@ -13,9 +37,17 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
+        <AddCommentForm
+            articleName={articleId}
+            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
