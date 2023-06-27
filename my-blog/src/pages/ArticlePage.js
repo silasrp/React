1a6d99d9c6b1ee39ca42580f010024ba27c2a5de
Silas Pereira <silasrp@gmail.com>
2023-06-27 19:09:41 +0100

    End of Authentication

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -8,26 +8,34 @@ import useUser from '../hooks/useUser';
 import articles from './article-content';
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({upvotes:0, comments:[]});
+    const [articleInfo, setArticleInfo] = useState({upvotes:0, comments:[], canUpvote: false });
+    const { canUpvote }= articleInfo;
     const { articleId } = useParams();
 
     const {user, isLoading} = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken: token } : {};
+            const response = await axios.get(`/api/articles/${articleId}`, { headers });
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
-        loadArticleInfo();
-    }, []);
+
+        if (isLoading) {
+            loadArticleInfo();
+        }
+    }, [isLoading, user]);
 
 
     const article = articles.find(article => article.name === articleId);
 
     // Make a request to the server that we want to upvote an article
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};        
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, {headers});
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     };
@@ -42,7 +50,7 @@ const ArticlePage = () => {
         <h1>{article.title}</h1>
         <div className="upvotes-section">
             {user 
-                ? <button onClick={addUpvote}>Upvote</button>
+                ? <button onClick={addUpvote}>{canUpvote ? 'Upvote' : 'Already Upvoted'}</button>
                 : <button>Log in to upvote</button>}
             <p>This article has {articleInfo.upvotes} upvote(s)</p>
         </div>
